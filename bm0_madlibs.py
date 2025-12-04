8b7c718986304a76bdb524dcee3846528375ed5e
arithehehe <ariellej26@nycstudents.net>
2025-12-04 11:12:26 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,2 +1,30 @@
-print("hello world")
+import random
 
+story=random.randint(0,1)
+
+print("Create your own story!")
+
+
+
+print("input noun:")
+noun=input()
+
+print("input verb")
+verb=input()
+
+print("input adjective")
+adjective=input()
+
+if story==1:
+	print(f'''
+Once apon a time a {noun} whom was a mage decied to go on a long walk, 
+on this walk they discovered a dragon which was {adjective}. 
+This dragon looked as mighty as a shark and was as sttong as fifteen bears.
+This mage knew this dragon was a large threat and slayed the dragon by {verb}
+		''')
+else:
+	print(f'''
+Before i go to bed i always {verb}, 
+this action always makes me feel vary {adjective}.
+I also always say goodnight to my {noun}
+		''')
